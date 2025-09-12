ac237538b9d97d654d494ee9f4e22fc9cd6ab7ad
Karasik011 <sergii.kozko@gmail.com>
2025-09-13 02:12:59 +0300

    remake with async

--- main.py
+++ main.py
@@ -1,24 +1,25 @@
+import aiohttp
 import requests
 import pandas as pd
 import asyncio
 import matplotlib.pyplot as plt
-
+import tracemalloc
+tracemalloc.start()
 from constants import SUMMONER_INFO, MATCH_HISTORY, MATCH_STATS
-def main():
-    summoner_name = 'KaRasIK4'
-    tag_name = 'EUW'
-    response = requests.get(SUMMONER_INFO.format(summoner_name, tag_name))
-    if response.status_code == 200:
-        data = response.json()
-        puu_id = data['puuid']
-        match_history_response = requests.get(MATCH_HISTORY.format(puu_id))
-        if match_history_response.status_code == 200:
-            matches = match_history_response.json()
-            games = []
-            for i in matches:
-                match_info_response = requests.get(MATCH_STATS.format(i))
-                if match_info_response.status_code == 200:
-                    match_data = match_info_response.json()
+async def main(summoner_name, tag_name):
+    newTable = []
+    async with aiohttp.ClientSession() as session:
+        async with session.get(SUMMONER_INFO.format(summoner_name, tag_name)) as response:
+            if response.status == 200:
+                data = await response.json()
+                puu_id = data['puuid']
+        async with session.get(MATCH_HISTORY.format(puu_id)) as id_response:
+                if id_response.status == 200:
+                    matches = await id_response.json()
+        for i in matches:
+                async with session.get(MATCH_STATS.format(i)) as match_info_response:
+                    if match_info_response.status == 200:
+                        match_data = await match_info_response.json()
                     if match_data['info']['gameMode'] == 'CLASSIC':
                         participants = match_data['metadata']['participants']
                         player_index = participants.index(puu_id)
@@ -28,7 +29,7 @@ def main():
                         deaths = player_info['deaths']
                         assists = player_info['assists']
                         if deaths != 0:
-                            kda = (kills + assists) // deaths
+                            kda = (kills + assists) / deaths
                         else:
                             kda = kills + assists
                         team_position = player_info['teamPosition']
@@ -37,17 +38,24 @@ def main():
                         else:
                             win_status = 'Loss'
                         match_type = match_data['info']['gameMode']
-                        stats = {'Champion': champion_name, 'Kills': kills, 'Deaths': deaths, 'Assists': assists,'KDA': kda, 'Position': team_position, 'Win/Lose': win_status, 'Type': match_type}
-                        games.append(stats)
+                        stats = {'Champion': champion_name, 'Kills': kills, 'Deaths': deaths, 'Assists': assists,'KDA': kda, 'Position': team_position, 'Win Status': win_status, 'Type': match_type}
+                        newTable.append(stats)
+                        MatchData = pd.DataFrame(newTable)
                         print(stats)
-                    else:
-                        print('Фан режим')
-            game_data = pd.DataFrame(games)
-            print(game_data.head())
-            plt.hist(game_data['Kills'], bins=30)
-            plt.show()
+                        continue
+        print(MatchData.head())
 
 
 
 
-main()
+asyncio.run(main('Karasik4', 'EUW'))
+#Хочу выводить КДА на дистанции 20 игр с помощью гистограмы и высчитать среднее KDA
+#def kda(summoner_name, tag):
+    #id = requests.get(SUMMONER_INFO.format(summoner_name, tag))
+    #if id.status_code == 200:
+        #data = id.json()
+        #puu_id = data['puuid']
+
+
+
+

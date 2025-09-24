443f63a5c8bd3237c55b43d7dd0287f81d3579d2
Karasik011 <sergii.kozko@gmail.com>
2025-09-24 03:55:42 +0300

    .

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ import tracemalloc
 tracemalloc.start()
 from constants import SUMMONER_INFO, MATCH_HISTORY, MATCH_STATS
 
+
+newTable = []
 async def request(session, url):
     async with session.get(url) as response:
         if response.status == 200:
@@ -14,44 +16,58 @@ async def request(session, url):
         else:
             return print(f'Mistake:{response.status}')
 
-newTable = []
+
+async def match_data(session, match_id, puuid):
+    match_stats = await request(session, MATCH_STATS.format(match_id))
+    if match_stats['info']['gameMode'] != 'CLASSIC':
+        return None
+    participants = match_stats['metadata']['participants']
+    player_index = participants.index(puuid)
+    player_info = match_stats['info']['participants'][player_index]
+    champion_name = player_info['championName']
+    kills = player_info['kills']
+    deaths = player_info['deaths']
+    assists = player_info['assists']
+    kda = (kills + assists) / deaths if deaths > 0 else (kills + assists)
+    team_position = player_info['teamPosition']
+    win_status = 'Win' if player_info['win'] else 'Lose'
+    match_type = match_stats['info']['gameMode']
+    stats = {'Champion': champion_name, 'Kills': kills, 'Deaths': deaths, 'Assists': assists, 'KDA': kda,
+             'Position': team_position, 'Win Status': win_status, 'Type': match_type}
+    return stats
+
+
+
+
+
+
+
+
+
 async def main(summoner_name, tag_name, count):
     async with aiohttp.ClientSession() as session:
         data = await request(session, SUMMONER_INFO.format(summoner_name, tag_name))
         puu_id = data['puuid']
-        matches = await request(session, MATCH_HISTORY.format(puu_id, count))
-        for i in matches:
-                async with session.get(MATCH_STATS.format(i)) as match_info_response:
-                    if match_info_response.status == 200:
-                        match_data = await match_info_response.json()
-                    if match_data['info']['gameMode'] == 'CLASSIC':
-                        participants = match_data['metadata']['participants']
-                        player_index = participants.index(puu_id)
-                        player_info = match_data['info']['participants'][player_index]
-                        champion_name = player_info['championName']
-                        kills = player_info['kills']
-                        deaths = player_info['deaths']
-                        assists = player_info['assists']
-                        kda = (kills + assists) / deaths if deaths > 0 else kda == kills + assists
-                        team_position = player_info['teamPosition']
-                        win_status = 'Win' if player_info['win'] else 'Lose'
-                        match_type = match_data['info']['gameMode']
-                        stats = {'Champion': champion_name, 'Kills': kills, 'Deaths': deaths, 'Assists': assists,'KDA': kda, 'Position': team_position, 'Win Status': win_status, 'Type': match_type}
-                        newTable.append(stats)
-                        print(stats)
-                        continue
+        match_id = await request(session, MATCH_HISTORY.format(puu_id, count))
+        dicts = [match_data(session, match_id, puu_id) for match_id in match_id]
+        outcome = await asyncio.gather(*dicts)
+        result = [i for i in outcome if i]
+        newTable.append(result)
+        print(result)
+        return result
+
 
 asyncio.run(main('Karasik4', 'EUW', 20))
-MatchData = pd.DataFrame(newTable)
-MatchData.head()
+#MatchData = pd.DataFrame(newTable)
+#MatchData.head()
 
 
 
 
 #Хочу выводить КДА на дистанции 100 игр с помощью гистограмы и высчитать среднее KDA
 
-plt.bar(MatchData['Position'],MatchData['KDA'])
-plt.show()
+#plt.bar(MatchData['Position'],MatchData['KDA'])
+#plt.show()
 
 
 
